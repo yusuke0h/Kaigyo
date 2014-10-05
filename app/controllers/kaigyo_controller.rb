aac4402fbd7c0409f5dd91880be314978296d331
Yusuke Hosoi <yusuke0.hosoi@gmail.com>
2014-10-05 20:30:25 +0900

    ひと通りの実装

--- app/controllers/kaigyo_controller.rb
+++ app/controllers/kaigyo_controller.rb
@@ -1,15 +1,41 @@
 class KaigyoController < ApplicationController
   def index
     if params[:data]
-      lines = []
-      text = params[:data][:text].strip.gsub("\n", "").gsub("\r", "")
       num = params[:data][:num].to_i
-      0.upto(text.size / num) do |term|
-        lines << text[num*term..num*(term+1)-1].strip
+      if num == 0
+        num = 30
+      end
+
+      lines = []
+      text = params[:data][:text].strip.gsub("\r", "")
+      text.split("\n").each do |line|
+        if line.size / num > 0
+          0.upto(line.size / num) do |term|
+            lines << line[num*term..num*(term+1)-1]
+          end
+        else
+          lines << line
+        end
       end
       @processed_text = lines.join("<br>")
     end
-    # a
   end
 
 end
+
+
+
+      # text.split(/^$/).each do |line|
+      #   line = line.gsub("\n", "")
+      #   if line.size / num > 0
+      #     0.upto(line.size / num) do |term|
+      #       lines << line[num*term..num*(term+1)-1].strip
+      #     end
+      #   else
+      #     lines << line.strip
+      #   end
+      # end
+
+      # 0.upto(text.size / num) do |term|
+      #   lines << text[num*term..num*(term+1)-1].strip
+      # end
