fcbcf2a9040a498df076140e2e60b4a842f44e92
Yosuke Kabuto <bluewhale1982@gmail.com>
2015-12-03 13:52:30 +0900

    Remove uselsee comments

--- app/controllers/kaigyo_controller.rb
+++ app/controllers/kaigyo_controller.rb
@@ -21,20 +21,3 @@ class KaigyoController < ApplicationController
     @processed_text = lines.join("<br>")
   end
 end
-
-
-
-      # text.split(/^$/).each do |line|
-      #   line = line.gsub("\n", "")
-      #   if line.size / num > 0
-      #     0.upto(line.size / num) do |term|
-      #       lines << line[num*term..num*(term+1)-1].strip
-      #     end
-      #   else
-      #     lines << line.strip
-      #   end
-      # end
-
-      # 0.upto(text.size / num) do |term|
-      #   lines << text[num*term..num*(term+1)-1].strip
-      # end
