16c8f321a2184c1ac513935be50282ae7ed3ef09
Yosuke Kabuto <bluewhale1982@gmail.com>
2015-12-03 20:23:56 +0900

    Create KoigyoController#wrong_params? to check params

--- app/controllers/kaigyo_controller.rb
+++ app/controllers/kaigyo_controller.rb
@@ -1,6 +1,14 @@
 class KaigyoController < ApplicationController
   def index
-    return if params[:data][:num].to_i < 1
+    return if wrong_params?
     @processed_text = Word.new(params[:data]).format
+  end
+
+  private
+
+  def wrong_params?
+    params[:data].blank? ||
+      params[:data][:num].blank? ||
+      params[:data][:num].to_i < 1
   end
 end
