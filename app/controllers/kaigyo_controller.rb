338a49acf4184d0b800cefda2d503b3897452a0c
Yosuke Kabuto <bluewhale1982@gmail.com>
2015-12-03 17:07:27 +0900

    Remove all codes in index action because Word class takes care of them

--- app/controllers/kaigyo_controller.rb
+++ app/controllers/kaigyo_controller.rb
@@ -1,22 +1,4 @@
 class KaigyoController < ApplicationController
   def index
-    data = params[:data]
-    return unless data
-    num = data[:num].to_i == 0 ? 30 : data[:num].to_i
-
-    formatted_text =
-      data[:text]
-      .strip
-      .gsub("\r", '')
-      .split("\n")
-
-    lines = []
-
-    formatted_text.each do |line|
-      line
-        .scan(/.{1,#{num}}/)
-        .map { |chopped_line| lines << chopped_line }
-    end
-    @processed_text = lines.join('<br>')
   end
 end
