c2779cddcecef05353a17a147716d16fec512946
Yosuke Kabuto <bluewhale1982@gmail.com>
2015-12-03 17:08:57 +0900

    Create instance of Word to assign value to the variable @processed_text

--- app/controllers/kaigyo_controller.rb
+++ app/controllers/kaigyo_controller.rb
@@ -1,4 +1,9 @@
 class KaigyoController < ApplicationController
   def index
+    return if params[:data][:num].to_i < 1
+    @processed_text =
+      Word.new(params[:data])
+      .format_text
+      .push_to_lines
   end
 end
