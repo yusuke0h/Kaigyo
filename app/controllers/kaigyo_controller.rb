d40c042bc015d07fdda361cb99e45c8e43849534
Yosuke Kabuto <bluewhale1982@gmail.com>
2015-12-03 13:49:57 +0900

    Use guard clause

--- app/controllers/kaigyo_controller.rb
+++ app/controllers/kaigyo_controller.rb
@@ -1,26 +1,24 @@
 class KaigyoController < ApplicationController
   def index
-    if params[:data]
-      num = params[:data][:num].to_i
-      if num == 0
-        num = 30
-      end
-
-      lines = []
-      text = params[:data][:text].strip.gsub("\r", "")
-      text.split("\n").each do |line|
-        if line.size / num > 0
-          0.upto(line.size / num) do |term|
-            lines << line[num*term..num*(term+1)-1]
-          end
-        else
-          lines << line
-        end
-      end
-      @processed_text = lines.join("<br>")
+    return unless params[:data]
+    num = params[:data][:num].to_i
+    if num == 0
+      num = 30
     end
-  end
 
+    lines = []
+    text = params[:data][:text].strip.gsub("\r", "")
+    text.split("\n").each do |line|
+      if line.size / num > 0
+        0.upto(line.size / num) do |term|
+          lines << line[num*term..num*(term+1)-1]
+        end
+      else
+        lines << line
+      end
+    end
+    @processed_text = lines.join("<br>")
+  end
 end
 
 
